96a85857a58b01ec160178deea796ad9ea26b5bc
eladmaa <89908683+eladmaa@users.noreply.github.com>
2026-02-08 03:45:48 +0200

    yggyoi

--- app/src/main/java/com/example/whackamole/MainActivity.java
+++ app/src/main/java/com/example/whackamole/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.whackamole;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.gridlayout.widget.GridLayout;
@@ -12,22 +14,53 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
    ImageView img;
+   Handler handler;
     GridLayout Mygrid;
-    private ArrayList<ImageView> holes = new ArrayList<>();
+    Random rnd;
+    private ArrayList<ImageView> holes;
+    private Runnable molesRunnable;
     final int holesnum = 9;
+    int currentmoleindex = -1;
+    int moleindex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Initialize();
         createboard();
+        handler.post(molesRunnable);
+
+    }
+
+    private void Initialize() {
+        holes = new ArrayList<>();
+        rnd = new Random();
+        Mygrid = findViewById(R.id.gridLay);
+        handler = new Handler();
+        molesRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentmoleindex != -1) {
+                    holes.get(currentmoleindex).setImageResource(R.drawable.hole);
+
+
+                }
+                moleindex = rnd.nextInt(holesnum);
+                holes.get(moleindex).setImageResource(R.drawable.mole);
+                currentmoleindex = moleindex;
+                handler.postDelayed(this, 1000);
+
+            }
+        };
+
     }
 
     private void createboard() {
         int column = (int)Math.sqrt(holesnum);
-        Mygrid = findViewById(R.id.gridLay);
         for(int i =0;i<holesnum; i++){
             int col = i % column;
             int row = i / column;
@@ -41,6 +74,14 @@ public class MainActivity extends AppCompatActivity {
             params.height = 250;
             params.setMargins(10,10,10,10);
             hole.setLayoutParams(params);
+            hole.setTag(i);
+            hole.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int clickhole = (int) v.getTag();
+                }
+            });
+            holes.add(hole);
             Mygrid.addView(hole);
         }
 
